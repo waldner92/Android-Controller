d4e3043d3b3a2bb55d25750e62714878368ee731
Adrià Mallorqui <ls28953@alleurl.edu>
2018-04-07 13:43:44 +0200

    Added the other 4 activities (empty)

---
+++ app/src/main/java/com/android/metg2/androidcontroller/MazeActivity.java
@@ -0,0 +1,13 @@
+package com.android.metg2.androidcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maze);
+    }
+}
